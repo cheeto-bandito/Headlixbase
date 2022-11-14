5ca3c06eb774dc8baf588bcc7f023175b713113b
Chet Potvin <CPotvin@degdigital.com>
2022-11-14 14:28:41 -0600

    Added Helix module template to scaffolding

---
+++ scaffolding/headlixbase.module/helixlayer/modulename/website/DI/RegisterContainer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace headlixbase.helixlayer.modulename.DI
+{
+    public class RegisterContainer : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+
+        }
+    }
+}
